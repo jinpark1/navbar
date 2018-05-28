057339da300c1e6cc45035f4a5a2cfca0c5dab80
unknown <jempsey@gmail.com>
2018-05-28 11:26:48 -0700

    nav bar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <div className="logo">LOGO</div>
+          <div className="menu phone">MENU</div>
+          <div className="menu desktop">ABOUT DOWNLOAD CONTACT</div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
